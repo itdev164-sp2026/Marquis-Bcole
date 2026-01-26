aa79b2efc154a2b917a778ffb828df0227fa6105
Marquis <browncom@gmatc.matc.edu>
2026-01-26 14:24:57 -0600

    completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Marquis Gatsby blog`,
+    description: ``,
+    author: `Marquis cole`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: "Marquis Cole",
+      company: "Blogs Inc",
+      address: "123 W South Park ave"
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
